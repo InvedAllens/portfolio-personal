301e767a1f8008f858f0417b4aee98742ab0701c
AldoAllens <aldoallens1@gmail.com>
2023-03-06 10:21:30 -0600

    se agregaron validaciones simples al formulario y se retocaron secciones

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,75 +1,74 @@
+import {validarInput} from "./validaciones.js";
 (()=>{
-document.addEventListener("DOMContentLoaded",()=>{
-    iniciarApp();
-});
-function iniciarApp(){
-    navegacion();
-    navegacionFija();
-}
-function navegacionFija(){
-    const header=document.querySelector(".header");
-    const sobreMi=document.querySelector("#sobreMi");
-    const body=document.querySelector("body");
-    window.addEventListener("scroll",e=>{
-        if(sobreMi.getBoundingClientRect().top<0){
-            header.classList.add("fijo");
-            body.classList.add("body-scroll");
-
-        }else{
-            header.classList.remove("fijo");
-            body.classList.remove("body-scroll");
-        }
+    document.addEventListener("DOMContentLoaded",()=>{
+        iniciarApp();
     });
-}
+    function iniciarApp(){
+        const fecha=document.querySelector("#fecha");
+        //const fechaActual=new Date();
+        const year=new Date().getFullYear();
+        fecha.innerHTML=year;
+        navegacion();
+        navegacionFija();
+        formulario();
+    }
+    function navegacionFija(){
+        const header=document.querySelector(".header");
+        const sobreMi=document.querySelector("#sobreMi");
+        const body=document.querySelector("body");
+        window.addEventListener("scroll",e=>{
+            if(sobreMi.getBoundingClientRect().top<0){
+                header.classList.add("fijo");
+                body.classList.add("body-scroll");
 
-function navegacion(){
-    const secciones=document.querySelectorAll(".seccion");
-    const itemsNav=document.querySelectorAll(".enlace-nav");
-    const height=window.innerHeight*.1;
-    let seccionActual;
-    
-    
-    // const funcionObserver=entries=>{
-    //     entries.forEach(entry =>{
-    //         if(entry.isIntersecting){
-    //             console.log(entry.target)
-    //             const seccionActual=Array.from(itemsNav).find(item => item.dataset.url===entry.target.id);
-    //             console.log(seccionActual);
-    //             if(seccionActual){
-    //                 seccionActual.classList.add("activo");
-    //             }               
-    //             for(const item of itemsNav){
-    //                 if(item!=seccionActual){
-    //                     item.classList.remove("activo");
-    //                 }
-    //             } 
-    //         }
-    //     });
-    // }
-
-    const observer=new IntersectionObserver((entries,observer)=>{
-        entries.forEach(entry =>{
-            if(entry.isIntersecting){
-                console.log(entry.target)
-                seccionActual=Array.from(itemsNav).find(item => item.dataset.url===entry.target.id);
-                console.log(seccionActual);
-                if(seccionActual){
-                    seccionActual.classList.add("activo");
-                }               
-                for(const item of itemsNav){
-                    if(item!=seccionActual){
-                        item.classList.remove("activo");
-                    }
-                } 
+            }else{
+                header.classList.remove("fijo");
+                body.classList.remove("body-scroll");
             }
         });
-    },{
-        root:null,
-        rootMargin:'-20px 0px 0px 0px',
-        threshold:0.7
-    });
+    }
 
-    secciones.forEach(seccion => observer.observe(seccion));
-    
-}
+    function navegacion(){
+        const secciones=document.querySelectorAll(".seccion");
+        const itemsNav=document.querySelectorAll(".enlace-nav");
+        const height=window.innerHeight*.1;
+        let seccionActual;
+
+        const observer=new IntersectionObserver((entries,observer)=>{
+            entries.forEach(entry =>{
+                if(entry.isIntersecting){
+                    console.log(entry.target)
+                    seccionActual=Array.from(itemsNav).find(item => item.dataset.url===entry.target.id);
+                    console.log(seccionActual);
+                    if(seccionActual){
+                        seccionActual.classList.add("activo");
+                    }               
+                    for(const item of itemsNav){
+                        if(item!=seccionActual){
+                            item.classList.remove("activo");
+                        }
+                    } 
+                }
+            });
+        },{
+            root:null,
+            rootMargin:'-20px 0px 0px 0px',
+            threshold:0.7
+        });
+
+        secciones.forEach(seccion => observer.observe(seccion));
+        
+    }
+
+    function formulario(){
+        const inputs=document.querySelectorAll("input");
+        inputs.forEach(input =>{
+        input.addEventListener("blur",validarInput);
+        const textsarea=document.querySelectorAll("textarea");
+        textsarea.forEach(textarea=>{
+            textarea.addEventListener("blur",validarInput);
+        });
+    });
+    }
+
 })();
---
+++ assets/js/validaciones.js
@@ -0,0 +1,71 @@
+const mensajesError={
+    nombre:{
+        valueMissing:"Este campo no puede estar vacio",
+    },
+    email:{
+        valueMissing:"Este campo no puede estar vacio",
+        typeMismatch:"El correo no es valido"
+    },
+    asunto:{
+        valueMissing:"Este campo no puede estar vacio",
+        patternMismatch:"Al menos 6 caracteres, máximo 12, debe contener una letra minúscula, una letra mayúscula, un número y no puede contener caracteres especiales."
+    },
+    mensaje:{
+        valueMissing:"Este campo no puede estar vacio"
+    }
+
+    
+};
+const verificarTipoInput={
+    nacimiento:(e)=>{validarEdad(e);}
+}
+export function validarInput(e){
+
+    const tipoInput=e.target.dataset.tipo;
+    if(verificarTipoInput[tipoInput]){
+        verificarTipoInput[tipoInput](e);
+    }
+    if(e.target.validity.valid){
+        e.target.parentElement.classList.remove("input-container--invalid");
+        console.log(e.target.validity);
+    }else{
+        e.target.parentElement.classList.add("input-container--invalid");
+        e.target.parentElement.querySelector(".input-message-error").innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#e73d3d" class="bi bi-exclamation-circle" viewBox="0 0 16 16">
+        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+        <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z"/>
+      </svg> `+mostrarMensajeError(tipoInput,e.target);
+    }
+}
+
+function mostrarMensajeError(tipoInput,input){
+    //console.log(mensajesError);
+    console.log(input.validity);
+    console.log(input.value);
+    let mensajes='';
+    console.log(mensajesError[tipoInput])
+    for(const [clave,valor] of Object.entries(mensajesError[tipoInput])){
+            if(input.validity[clave]){
+                mensajes=valor;
+            }
+        
+    }
+    //console.log(mensajes);
+    return mensajes;
+}
+
+
+function validarEdad(e){
+    const inputFecha=new Date(e.target.value);
+    const fechaCliente=new Date(inputFecha.getUTCFullYear()+18,inputFecha.getUTCMonth(),inputFecha.getUTCDate());
+    const fechaActual=new Date();
+    const esMayorEdad=(fechaCliente<=fechaActual);
+    if(!esMayorEdad){
+         e.target.setCustomValidity("Debes de ser mayor de 18 años para registrarte");
+         e.target.value=null;
+    }else{
+        e.target.setCustomValidity('');
+    }
+
+    console.log(fechaCliente);
+    console.log(esMayorEdad);
+}
