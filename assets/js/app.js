9b8a11521ed08040bda74eb2386948dab466ce28
AldoAllens <aldoallens1@gmail.com>
2023-03-12 18:51:16 -0600

    utilizando gulp para minimizar archivos, migracion de datos a json

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,196 +1,135 @@
-import {validarInput} from "./validaciones.js";
-(()=>{
-    document.addEventListener("DOMContentLoaded",()=>{
+
+import{
+    formulario
+} from "./formulario.js"
+(() => {
+    document.addEventListener("DOMContentLoaded", () => {
         iniciarApp();
     });
 
-    function iniciarApp(){
-        const fecha=document.querySelector("#fecha");
+    function iniciarApp() {
+        const fecha = document.querySelector("#fecha");
         //const fechaActual=new Date();
-        const year=new Date().getFullYear();
-        let size=screen.width;
-        console.log(size>=768);
-        fecha.innerHTML=year;
-        if(size>=768){
+        const year = new Date().getFullYear();
+        obtenerMisDatos();
+        let size = screen.width;
+        fecha.innerHTML = year;
+        if (size >= 768) {
             navegacionFija();
             navegacion();
-        }else{
+        } else {
             navegacionMobile();
         }
-        
+
         formulario();
-        window.addEventListener('resize', function(event) {
-            size=screen.width;
-            if(size>=768){
+        window.addEventListener('resize', function (event) {
+            size = screen.width;
+            if (size >= 768) {
                 navegacionFija();
                 navegacion();
-            }else{
+            } else {
                 navegacionMobile();
             }
-        },true);
+        }, true);
         keyboard();
     }
-    function navegacionFija(){
-        const header=document.querySelector(".header");
-        const sobreMi=document.querySelector("#sobreMi");
-        const body=document.querySelector("body");
-        window.addEventListener("scroll",e=>{
-            if(sobreMi.getBoundingClientRect().top<0){
+    function obtenerMisDatos(){
+        const presentacionNombre=document.querySelector(".presentacion__ocupacion");
+        const presentacionDescripcion=document.querySelector(".presentacion__descripcion");
+        const sobreMI=document.querySelector(".sobreMi");
+        const sobreMiText=document.createElement("P");
+        const nombreProyectos=document.querySelectorAll(".proyecto__nombre");
+        const ProyectosTexto=document.querySelectorAll(".proyecto__texto");
+        let i=0;
+        sobreMI.appendChild(sobreMiText);
+        fetch("/data.json").then((response)=>{
+             const respuesta=response.json();
+             respuesta.then((datos)=>{
+                presentacionNombre.innerHTML=`${datos.presentacion["nombre"]} | <span>${datos.presentacion["ocupacion"]}</span>`;
+                presentacionDescripcion.innerHTML=`${datos.presentacion.descripcion}`;
+                sobreMiText.textContent=datos.sobreMi;
+                nombreProyectos.forEach((proyectoNombre)=>{
+                    proyectoNombre.textContent=datos.proyectos[i].nombre;
+                    ProyectosTexto[i].textContent=datos.proyectos[i].descripcion;
+                    i++;
+                });
+            })
+            
+            // presentacionNombre.value=response.json().nombre;
+            // console.log(presentacionNombre);
+        })
+
+    }
+
+    function navegacionFija() {
+        const header = document.querySelector(".header");
+        const sobreMi = document.querySelector("#sobreMi");
+        const body = document.querySelector("body");
+        window.addEventListener("scroll", e => {
+            if (sobreMi.getBoundingClientRect().top < 0) {
                 header.classList.add("fijo");
                 body.classList.add("body-scroll");
 
-            }else{
+            } else {
                 header.classList.remove("fijo");
                 body.classList.remove("body-scroll");
             }
         });
     }
 
-    function navegacion(){
-        const secciones=document.querySelectorAll(".seccion");
-        const itemsNav=document.querySelectorAll(".enlace-nav");
-        const height=window.innerHeight*.1;
+    function navegacion() {
+        const secciones = document.querySelectorAll(".seccion");
+        const itemsNav = document.querySelectorAll(".enlace-nav");
+        const height = window.innerHeight * .1;
         let seccionActual;
 
-        const observer=new IntersectionObserver((entries,observer)=>{
-            entries.forEach(entry =>{
-                if(entry.isIntersecting){
-                    console.log(entry.target)
-                    seccionActual=Array.from(itemsNav).find(item => item.dataset.url===entry.target.id);
-                    console.log(seccionActual);
-                    if(seccionActual){
+        const observer = new IntersectionObserver((entries, observer) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    seccionActual = Array.from(itemsNav).find(item => item.dataset.url === entry.target.id);
+                    // console.log(seccionActual);
+                    if (seccionActual) {
                         seccionActual.classList.add("activo");
-                    }               
-                    for(const item of itemsNav){
-                        if(item!=seccionActual){
+                    }
+                    for (const item of itemsNav) {
+                        if (item != seccionActual) {
                             item.classList.remove("activo");
                         }
-                    } 
+                    }
                 }
             });
-        },{
-            root:null,
-            rootMargin:'-17px 0px 0px 0px',
-            threshold:0.6
+        }, {
+            root: null,
+            rootMargin: '-17px 0px 0px 0px',
+            threshold: 0.6
         });
 
         secciones.forEach(seccion => observer.observe(seccion));
-        
+
     }
 
-    function formulario(){
-        const inputs=document.querySelectorAll("input");
-        inputs.forEach(input =>{
-        input.addEventListener("blur",validarInput);
-        const textsarea=document.querySelectorAll("textarea");
-        textsarea.forEach(textarea=>{
-            textarea.addEventListener("blur",validarInput);
-        });
-    });
-    const form=document.querySelector("#formulario");
-    form.addEventListener("submit",manejarSubmit);
-    }
-    async function manejarSubmit(event){
-        event.preventDefault();
-        const modal =document.querySelector(".modal");
-        const cerrarModal=document.querySelector(".cerrar-modal");
-        const modalContenido=document.querySelector(".modal__contenedor");
-        const modalBody=document.createElement("DIV");
-        modalBody.classList.add("modal__contenido");
-        modalContenido.appendChild(modalBody);
-
-        const loader=document.createElement("DIV");
-        const circle1=document.createElement("DIV");
-        circle1.classList.add("circle1");
-        const circle2=document.createElement("DIV");
-        circle2.classList.add("circle2");
-        const circle3=document.createElement("DIV");
-        circle3.classList.add("circle3");
-        loader.appendChild(circle1);
-        loader.appendChild(circle2);
-        loader.appendChild(circle3);
-
-        modalBody.appendChild(loader);
-
-        loader.classList.add("animacion-modal");
-
-        let mensaje=document.createElement("P");
-        mensaje.style.padding="1rem";
-        const iconoResponse=document.createElement("DIV");
-        const botonCerrarModal=document.createElement("DIV");
-        botonCerrarModal.classList.add("btn-cerrar-modal");
-        botonCerrarModal.textContent="Cerrar";
-        botonCerrarModal.style.color="#1f1d2e";
-        botonCerrarModal.style.fontWeight="500";
-
-        botonCerrarModal.addEventListener("click",()=>{
-            modal.style.visibility="hidden";
-            modalContenido.removeChild(modalBody);
-        });
-       
-        cerrarModal.addEventListener("click",()=>{
-            modal.style.visibility="hidden";
-            modalContenido.removeChild(modalBody);
-        });
-        modal.style.visibility="visible";
-        modal.style.zindex="100";
-        modal.style.opacity="1";
-
-        const formulario=new FormData(this);
-        let response;
-        //conexion con formspree para enviar mensajes 
-        response= await fetch(this.action,{
-            method:this.method,
-            body:formulario,
-            headers:{
-                'Accept':'application/json'
-            }
-        });
     
-        if(response.ok){
-            setTimeout(()=>{
-                modalBody.removeChild(loader);
-                iconoResponse.classList.add("icono-success");
-                modalBody.appendChild(iconoResponse);  
-                mensaje.textContent="Gracias por contactarme, respondere lo mas pronto que me sea posible ";
-                modalBody.append(mensaje);
-                modalBody.appendChild(botonCerrarModal);
-            },1500);
-            this.reset();
-        }else{
-            setTimeout(()=>{
-                modalBody.removeChild(loader);
-                iconoResponse.classList.add("icono-failed");
-                modalBody.appendChild(iconoResponse);              
-                mensaje.textContent="Hubo un problema con el envio del mensaje :/, intenta enviarme desde otra fuente o intenta nuevamente"
-                modalBody.appendChild(mensaje);
-                modalBody.appendChild(botonCerrarModal);
-                this.reset();
-            },1500);
-            
-        }
-    }
-    function navegacionMobile(){
-        const menu=document.querySelector(".menu-mobile");
-        const nav=document.querySelector("#navegacion");
-        menu.addEventListener("click",()=>{
-            console.log("haz hecho click sobre el menu");
+    function navegacionMobile() {
+        const menu = document.querySelector(".menu-mobile");
+        const nav = document.querySelector("#navegacion");
+        menu.addEventListener("click", () => {
             nav.classList.toggle("d-none");
         });
     }
-    function keyboard(){
+
+    function keyboard() {
         let keyBdNumber1;
         let keyBdNumber2;
-        setInterval(()=>{
-            keyBdNumber1=Math.round(Math.random()*16);
-            keyBdNumber2=Math.round(Math.random()*16);
-            if(keyBdNumber1!=0 && keyBdNumber2!=0){
-                const tecla1=document.querySelector(`#Rectangle${keyBdNumber1}`);
+        setInterval(() => {
+            keyBdNumber1 = Math.round(Math.random() * 16);
+            keyBdNumber2 = Math.round(Math.random() * 16);
+            if (keyBdNumber1 != 0 && keyBdNumber2 != 0) {
+                const tecla1 = document.querySelector(`#Rectangle${keyBdNumber1}`);
                 tecla1.classList.toggle('typingKB');
-                const tecla2=document.querySelector(`#Rectangle${keyBdNumber2}`);
-                tecla2.classList.toggle('typingKB');   
+                const tecla2 = document.querySelector(`#Rectangle${keyBdNumber2}`);
+                tecla2.classList.toggle('typingKB');
             }
-        },200)
+        }, 200)
     }
 
 })();
